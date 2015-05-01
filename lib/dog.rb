0b4d34a95cebdbf3acf4fdd542397e8389b58cfc
vicfriedman <victoria.m.friedman@gmail.com>
2015-05-01 17:58:19 -0400

    updated tests

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,3 @@
 class Dog
   # code goes here
-  def initialize(name)
-    @name = name
-    @mood = "nervous"
-  end
-
-  def name
-    @name
-  end
-
-  def mood=(mood)
-    @mood = mood
-  end
-
-  def mood
-    @mood
-  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,19 +1,3 @@
 class Fish
   # code goes here
-  def initialize(name)
-    @name = name
-    @mood = "nervous"
-  end
-
-  def mood=(mood)
-    @mood = mood
-  end
-
-  def mood
-    @mood
-  end
-
-  def name
-    @name
-  end
 end
